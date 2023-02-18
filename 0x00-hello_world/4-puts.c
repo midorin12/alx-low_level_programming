1076c03b00c9696a6539efd83395eacc6a3d8a6b
root <midorin12>
2023-02-18 06:49:41 -0800

    puts1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-/*
-*hello main function
+/**
+*main - entry point
+*return : always 0 (success)
 */
-int main(void)/*main func*/
+int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\n");/*puts func*/
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
