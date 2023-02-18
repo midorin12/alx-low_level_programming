62d92adfdbd51c0dc350d29fa018546f14aa64af
root <midorin12>
2023-02-18 06:37:28 -0800

    puts1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,8 @@
 /*
 *hello main function
 */
-int main(void)
+int main(void)/*main func*/
 {
-puts("\"Programming is like building a multilingual puzzle");/*puts func*/
-
+puts("\"Programming is like building a multilingual puzzle\n");/*puts func*/
+return (0);
+}
