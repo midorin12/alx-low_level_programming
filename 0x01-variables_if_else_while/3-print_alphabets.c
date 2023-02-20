f55d07bec674cd789458bfae611033d82feeb32c
root <midorin12>
2023-02-20 12:28:31 -0800

    alphabet(s)

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+*main - entry point
+*Return: 0 (Success)
+*/
+int main(void)
+{
+char c = 'a';
+
+for (c = 'a'; c <= 'z'; c++)
+{
+putchar(c);
+}
+for (c = 'A'; c <= 'Z'; c++)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
