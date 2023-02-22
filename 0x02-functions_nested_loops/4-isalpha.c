676464499c4976675bd423ba63c772d98bdcb1ce
root <midorin12>
2023-02-22 08:15:36 -0800

    alphabitic

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*_isalpha - entry point
+*@c: character
+*Return: 0 if non-alphabitic or 1 if alphabitic
+*/
+int _isalpha(int c)
+{
+if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+return (1);
+else
+return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,5 +5,6 @@ int _islower(int c);
 void print_alphabet(void);
 int _putchar(char c);
 void print_alphabet_x10(void);
+int _isalpha(int c);
 
 #endif
