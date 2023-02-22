56c5183ea06982498d0f50f27e3e3f60060a1efa
root <midorin12>
2023-02-22 07:17:21 -0800

    is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main"
+/**
+*_islower - entry point
+*@c: charcter
+*Return: 1 if lower or 0 if upper
+*/
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+return (1);
+else
+return (0);
+}
