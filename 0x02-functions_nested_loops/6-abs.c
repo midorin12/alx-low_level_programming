f647a04b27ac3aafefa47bdcc142fd8242113556
root <midorin12>
2023-02-23 04:22:41 -0800

    abs value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*_abs - entry point
+*@n: number
+*Return: 0 and abs value of the number n
+*/
+int _abs(int n)
+{
+int a;
+
+if (n < 0)
+{
+a = n * -1;
+printf("%d", a)
+}
+return (0);
+}
+
