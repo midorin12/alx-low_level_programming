b8346d45ad5ae17e7ae11485a1f753d2ddfd02a2
root <midorin12>
2023-02-25 09:33:47 -0800

    multiplies

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*mul - func that multiplies two integrs
+*@a: number 1
+*@b: number 2
+*Return: value of multi of two integers
+*/
+int mul(int a, int b)
+{
+int r;
+
+r = a * b;
+return (a);
+}
